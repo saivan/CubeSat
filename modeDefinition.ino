857be1e5ae4f7b97df70f7a4df90fd29a6aa70fb
martycheung <martin.cheung13@gmail.com>
2014-09-07 11:31:53 +1000

    Create modeDefinition.ino

---
+++ modeDefinition.ino
@@ -0,0 +1,13 @@
+//This will define each mode of operation
+//These functions should return a value to indicate whether there has been an error, 
+//in which case the gammaSat system will return to Safe mode
+
+int gammaSat::normalMode()
+{
+  
+}
+
+int gammaSat::normalMode()
+{
+  
+}

d816f10be5278dd896198909679286f04b96edce
martycheung <martin.cheung13@gmail.com>
2014-09-15 20:51:08 +1000

    Update getIMU.py

--- IMU/getIMU.py
+++ IMU/getIMU.py
@@ -12,13 +12,12 @@ while 1:
     line = ser.readline()
     angles = line.split(",")    # Split string into Yaw, Pitch, Roll (comma-separated)
     if len(angles) > 2:         # Check if String length is larger than 2 (doesnt seem to work if i dont do this)
-        try:
-            yaw = angles[0]     #Access each axis in the list 'angles'
-            pitch = angles[1]
-            roll = angles[2]
-            print "yaw =", yaw
-            print "pitch =", pitch
-            print "roll =", roll
-            print "\n"
-        except:
-            print "Invalid line"
+        yaw = angles[0]     #Access each axis in the list 'angles'
+        pitch = angles[1]
+        roll = angles[2]
+        print "yaw =", yaw
+        print "pitch =", pitch
+        print "roll =", roll
+        print "\n"
+    else:
+        print "Invalid line"

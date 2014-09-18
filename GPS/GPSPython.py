11ff49d3dfbe14f5c047a8a9ac1b0f02389e7da2
martycheung <martin.cheung13@gmail.com>
2014-09-18 15:25:06 +1000

    Some initial code to read and parse GPS Data

---
+++ GPS/GPSPython.py
@@ -0,0 +1,23 @@
+import serial
+
+#Setup UART Serial Port on the Pi.
+GPSSerial = serial.Serial("/dev/ttyAMA0",baudrate=9600,timeout=1)
+
+while 1:
+    #Random string inputted for testing
+    GPSData = "hfuhewrferi,$GPRMC, ewfwe,ewfe,wefew,wefe,few,ewf,ewf,wef,$GPVTG"
+
+    #GPSData = GPSSerial.readline() #serial read the GPS
+    #Find the index of the string where RMC data will begin and end 
+    startIndex = GPSData.index("$GPRMC")
+    endIndex = GPSData.index("$GPVTG")
+
+    #Create substring from the start index to the end index
+    RMCData = GPSData[startIndex+len("$GPRMC"):endIndex]
+
+    DataList = RMCData.split(",")
+    print DataList
+    GPSTime = DataList[0]
+    GPSLatitude = DataList[2]
+    GPSLongitude = DataList[4]
+    GPSDate = DataList[8]
